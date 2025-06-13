9526e10c7902f39dfd12f00efe5dadd017d7823c
nahuelcostadalto <nahuelcostadalto@hotmail.com>
2025-06-13 19:15:27 -0300

    Se arregla Id en proveedores
    Ya que no vamos a utilizar un proveedor unico con una "CI" o "Identificador" distinto se modifica para que sean todas las modificaciones por el ID del mismo.

--- backend/dominio/proveedores.py
+++ backend/dominio/proveedores.py
@@ -1,7 +1,8 @@
 from datetime import datetime, date
 class proveedores:
-    def __init__(self,nombre,contacto):
+    def __init__(self,id,nombre,contacto):
 
+        self.id=id
         self.nombre=nombre
         self.contacto=contacto
     
