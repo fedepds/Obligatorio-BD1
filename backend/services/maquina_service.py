b81bbcdd5de2fcbc32d212ca4f0c78883d2ac540
FrancoDisa <frankods2010@gmail.com>
2025-06-24 20:17:18 -0300

    Cambiados los get por database

--- backend/services/maquina_service.py
+++ backend/services/maquina_service.py
@@ -1,28 +1,24 @@
 from backend.dominio import Maquina
-from backend.connection import get_db_connection
+from backend.connection import DatabaseConnection
 
 def agregar_maquina(maquina):
     query = """INSERT INTO maquinas (id, modelo, id_cliente, ubicacion_cliente, costo_alquiler_mensual) 
                VALUES (%s, %s, %s, %s, %s)"""
     values = (maquina.id, maquina.modelo, maquina.id_cliente, maquina.ubicacion_cliente, maquina.costo_alquiler_mensual)
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query, values)
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
 
 def eliminar_maquina(maquina_id):
     query = """DELETE FROM maquinas WHERE id = %s"""
     values = (maquina_id,)
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query, values)
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
 
 def modificar_maquina(maquina, nuevos_datos):
     query = """UPDATE maquinas SET modelo = %s, id_cliente = %s, ubicacion_cliente = %s, costo_alquiler_mensual = %s 
@@ -30,20 +26,16 @@ def modificar_maquina(maquina, nuevos_datos):
     values = (nuevos_datos.modelo, nuevos_datos.id_cliente, nuevos_datos.ubicacion_cliente, 
               nuevos_datos.costo_alquiler_mensual, maquina.id)
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query, values)
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
 
 def obtener_maquinas():
     query = """SELECT * FROM maquinas"""
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return rows
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return rows
--- backend/services/tecnico_service.py
+++ backend/services/tecnico_service.py
@@ -1,48 +1,40 @@
 from backend.dominio import Tecnico
-from backend.connection import get_db_connection
+from backend.connection import DatabaseConnection
 
 def agregar_tecnico(tecnico):
     query = """INSERT INTO tecnicos (id, nombre, contacto) 
                VALUES (%s, %s, %s)"""
     values = (tecnico.id, tecnico.nombre, tecnico.contacto)
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query, values)
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
 
 def eliminar_tecnico(tecnico_id):
     query = """DELETE FROM tecnicos WHERE id = %s"""
     values = (tecnico_id,)
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query, values)
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
 
 def modificar_tecnico(tecnico, nuevos_datos_tecnico):
     query = """UPDATE tecnicos SET nombre = %s, contacto = %s 
                WHERE id = %s"""
     values = (nuevos_datos_tecnico.nombre, nuevos_datos_tecnico.contacto, tecnico.id)
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query, values)
-    connection.commit()
-    cursor.close()
-    connection.close()
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
+        connection.commit()
 
 def obtener_tecnicos():
     query = """SELECT * FROM tecnicos"""
     
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return rows
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return rows
