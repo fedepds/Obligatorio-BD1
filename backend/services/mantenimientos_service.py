8394d1e5566e81490b17e031a1075c71a3ee7e36
FrancoDisa <frankods2010@gmail.com>
2025-07-03 15:58:46 -0300

    Quitado id

--- backend/services/mantenimientos_service.py
+++ backend/services/mantenimientos_service.py
@@ -2,9 +2,9 @@ from backend.dominio import Mantenimiento
 from backend.connection import DatabaseConnection
 
 def agregar_mantenimiento(mantenimiento):
-    query = """INSERT INTO mantenimientos (id, id_maquina, id_tecnico, tipo, fecha, observaciones) 
-               VALUES (%s, %s, %s, %s, %s, %s)"""
-    values = (mantenimiento.id, mantenimiento.id_maquina, mantenimiento.id_tecnico, mantenimiento.tipo, mantenimiento.fecha, mantenimiento.observaciones)
+    query = """INSERT INTO mantenimientos (id_maquina, id_tecnico, tipo, fecha, observaciones) 
+               VALUES (%s, %s, %s, %s, %s)"""
+    values = (mantenimiento.id_maquina, mantenimiento.id_tecnico, mantenimiento.tipo, mantenimiento.fecha, mantenimiento.observaciones)
     
     with DatabaseConnection() as connection:
         cursor = connection.cursor()
